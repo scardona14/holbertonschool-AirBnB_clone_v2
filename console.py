bf4dd559d28bc19b5dfd4d0f98a3fdc58586bbeb
scardona14 <cardanasean123@gmail.com>
2024-03-26 21:18:36 -0400

    Fixed a bug

--- console.py
+++ console.py
@@ -81,7 +81,7 @@ class HBNBCommand(cmd.Cmd):
                         # _args = _args.replace('\"', '')
             line = ' '.join([_cmd, _cls, _id, _args])
 
-        except Exception as mess:
+        except Exception:
             pass
         finally:
             return line
