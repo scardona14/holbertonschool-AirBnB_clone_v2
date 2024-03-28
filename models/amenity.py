162df6d4f5169ab35fa75b4f7d8dd458870a727b
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-03-28 09:07:46 -0700

    added imports, orm to place

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
+class Amenity(BaseModel, Base):
+    """ The amenity class, contains state ID and name """
+    __tablename__ = 'amenities'
 
-class Amenity(BaseModel):
-    name = ""
+    name = Column(String(120), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity")
