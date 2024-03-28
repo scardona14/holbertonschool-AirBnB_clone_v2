4b7c01085d4ba34ab8d4c21737fade94bfd26052
scardona14 <cardanasean123@gmail.com>
2024-03-28 18:36:07 -0400

    Fixed pycodestyle

--- models/amenity.py
+++ models/amenity.py
@@ -6,7 +6,7 @@ from os import getenv
 
 
 class Amenity(BaseModel, Base):
-    """amenity class"""
+    """Amenity class"""
     __tablename__ = 'amenities'
     if getenv("HBNB_TYPE_STORAGE") == 'db':
         name = Column(String(128), nullable=False)
