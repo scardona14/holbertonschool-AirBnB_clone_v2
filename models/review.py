15a58d5ef67f0f993d23fd1c42239c494c42e623
JFFPtech <144933326+JFFPtech@users.noreply.github.com>
2024-03-28 06:15:15 -0700

    added orm imports, table info

--- models/review.py
+++ models/review.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
+class Review(BaseModel, Base):
+    """ Review class to store review and user information """
+    __tablename__ = "reviews"
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+    text = Column(String(1020), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
