cd9ce7e9e6b830280ab1d022d24314699f7f2565
scardona14 <cardanasean123@gmail.com>
2024-03-28 18:16:35 -0400

    Fixed errors on the console

--- console.py
+++ console.py
@@ -27,8 +27,12 @@ class HBNBCommand(cmd.Cmd):
                'State': State, 'City': City, 'Amenity': Amenity,
                'Review': Review
               }
-    
     dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
+    types = {
+             'number_rooms': int, 'number_bathrooms': int,
+             'max_guest': int, 'price_by_night': int,
+             'latitude': float, 'longitude': float
+            }
 
     def preloop(self):
         """Prints if isatty is false"""
@@ -115,25 +119,68 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        all_attr = ('id', 'created_at', 'updated_at', '__class__')
+        class_name = ''
+        class_pat = r'(?P<name>(?:[a-zA-Z]|_)(?:[a-zA-Z]|\d|_)*)'
+        class_match = re.match(class_pat, args)
+        objects = {}
+        # find the right Command syntax
+        if class_match is not None:
+            class_name = class_match.group('name')
+            str_param = args[len(class_name):].strip()
+            params = str_param.split(' ')
+            str_pat = r'(?P<t_str>"([^"]|\")*")'
+            fp_pat = r'(?P<t_float>[-+]?\d+\.\d+)'
+            int_pat = r'(?P<t_int>[-+]?\d+)'
+            full_pat = '{}=({}|{}|{})'.format(class_pat, str_pat,
+                                              fp_pat, int_pat)
+            # check syntax of key and value pairs
+            for param in params:
+                param_match = re.fullmatch(full_pat, param)
+                if param_match is not None:
+                    key = param_match.group('name')
+                    str_value = param_match.group('t_str')
+                    fp_value = param_match.group('t_float')
+                    int_value = param_match.group('t_int')
+                    # address the string value syntax
+                    if str_value is not None:
+                        objects[key] = str_value[1:-1].replace('_', ' ')
+                    # address the floating point value syntax
+                    if fp_value is not None:
+                        objects[key] = float(fp_value)
+                    # address the integer value syntax
+                    if int_value is not None:
+                        objects[key] = int(int_value)
+        else:
+            class_name = args
+        if not class_name:
             print("** class name missing **")
             return
-        
-        class_name, _, arg_str = args.partition(' ')
-
-        if class_name not in self.classes:
+        elif class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
-        try:
-            obj_dict = eval("{" + arg_str + "}")
-        except Exception as e:
-            print("** invalid syntax: {}".format(e))
-            return
-        
-        new_obj = self.classes[class_name](**obj_dict)
-        new_obj.save()
-        print(new_obj.id)
+        # save parsed objects using mysqldb'
+        if os.getenv("HBNB_TYPE_STORAGE") == 'db':
+            if not hasattr(objects, 'id'):
+                objects['id'] = str(uuid.uuid4())
+
+            if not hasattr(objects, 'created_at'):
+                objects['created_at'] = str(datetime.now())
+
+            if not hasattr(objects, 'updated_at'):
+                objects['updated_at'] = str(datetime.now())
+            # save to db storage
+            new_attr = HBNBCommand.classes[class_name](**objects)
+            new_attr.save()
+            print(new_attr.id)
+        else:
+            # save to filestorage
+            new_attr = HBNBCommand.classes[class_name]()
+            for key, value in objects.items():
+                if key not in all_attr:
+                    setattr(new_attr, key, value)
+            new_attr.save()
+            print(new_attr.id)
 
     def help_create(self):
         """ Help information for the create method """
@@ -141,26 +188,30 @@ class HBNBCommand(cmd.Cmd):
         print("[Usage]: create <className>\n")
 
     def do_show(self, args):
-        """ Shows an individual instance of a class """
-        class_name, _, obj_id = args.partition(' ')
+        """ Method to show an individual object """
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
 
-        if not class_name:
+        # guard against trailing args
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
+
+        if not c_name:
             print("** class name missing **")
             return
-        
-        if class_name not in self.classes:
+
+        if c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
-        if not obj_id:
+
+        if not c_id:
             print("** instance id missing **")
             return
-        
-        key = class_name + "." + obj_id
 
+        key = c_name + "." + c_id
         try:
-            obj = storage.all()[key]
-            print(obj)
+            print(storage.all()[key])
         except KeyError:
             print("** no instance found **")
 
@@ -171,24 +222,28 @@ class HBNBCommand(cmd.Cmd):
 
     def do_destroy(self, args):
         """ Destroys a specified object """
-        class_name, _, obj_id = args.partition(' ')
+        new = args.partition(" ")
+        c_name = new[0]
+        c_id = new[2]
+        if c_id and ' ' in c_id:
+            c_id = c_id.partition(' ')[0]
 
-        if not class_name:
+        if not c_name:
             print("** class name missing **")
             return
-        
-        if class_name not in self.classes:
+
+        if c_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        
-        if not obj_id:
+
+        if not c_id:
             print("** instance id missing **")
             return
-        
-        key = class_name + "." + obj_id
+
+        key = c_name + "." + c_id
 
         try:
-            del storage.all()[key]
+            storage.delete(storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -203,14 +258,17 @@ class HBNBCommand(cmd.Cmd):
         print_list = []
 
         if args:
-            class_name = args.split(' ')[0]  # remove possible trailing args
-            if class_name not in self.classes:
+            args = args.split(' ')[0]  # remove possible trailing args
+            if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            print_list = [str(obj) for obj in storage.all(args).values() if type(obj).__name__ == class_name]
+            for k, v in storage.all().items():
+                if k.split('.')[0] == args:
+                    print_list.append(str(v))
         else:
-            print_list = [str(obj) for obj in storage.all().values()]
-        
+            for k, v in storage.all().items():
+                print_list.append(str(v))
+
         print(print_list)
 
     def help_all(self):
