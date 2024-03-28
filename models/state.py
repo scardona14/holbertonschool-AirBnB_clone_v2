89d3b7aa38b6f292203f73be2a7a231d5242552a
scardona14 <cardanasean123@gmail.com>
2024-03-28 19:49:43 -0400

    Fixed pycodestyle

--- models/state.py
+++ models/state.py
@@ -18,13 +18,13 @@ class State(BaseModel, Base):
         name = ""
 
     def __init__(self, *args, **kwargs):
-        """initializes state"""
+        """Initializes state"""
         super().__init__(*args, **kwargs)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """relationship between city and state"""
+            """Relationship between city and state"""
             from models import storage
             new_city = []
             all_cities = storage.all(City)
