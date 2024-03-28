3776e5168a6c67fc7e28993a079534071e8d68b1
scardona14 <cardanasean123@gmail.com>
2024-03-28 19:58:02 -0400

    Changed the user file fixed errors

--- models/user.py
+++ models/user.py
@@ -2,7 +2,6 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from os import getenv
 from sqlalchemy.orm import relationship
 
 
