61f2a55f6d2c096465bd83e6df62fb7eba85feb9
scardona14 <cardanasean123@gmail.com>
2024-03-28 20:08:20 -0400

    Added commets to the fucntions

--- models/user.py
+++ models/user.py
@@ -1,11 +1,20 @@
+#!/usr/bin/python3
+""" User module """
+
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
+    """ User class """
     __tablename__ = 'users'
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+
+    places = relationship('Place', backref='user',
+                            cascade='all, delete, delete-orphan')
+    reviews = relationship('Review', backref='user',
+                            cascade='all, delete, delete-orphan')
