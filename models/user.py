78d59efd0192a511905741c0499634ca071f1f75
scardona14 <cardanasean123@gmail.com>
2024-03-28 20:02:00 -0400

    Changed the user file since it had some errors

--- models/user.py
+++ models/user.py
@@ -2,18 +2,23 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
 
-    places = relationship("Place", backref="user",
-                          cascade="all, delete, delete-orphan")
-    reviews = relationship("Review", backref="user",
-                           cascade="all, delete, delete-orphan")
+    def __init__(self, *args, **kwargs):
+        """Initializes user"""
+        super().__init__(*args, **kwargs
