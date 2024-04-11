f02d06a0175dcab01e0e59083db93cff2b160c87
scardona14 <cardanasean123@gmail.com>
2024-04-11 14:07:01 -0400

    Fixing code that is not broken is a waste of time.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application
-listening on 0.0.0.0, port 5000
-"""
+"""Start a Flask web application."""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,41 +7,41 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Function that displays Hello HBNB!"""
+    """Display “Hello HBNB!”."""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Function that displays HBNB"""
+    """Display “HBNB”."""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def ctext(text):
-    """Function that displays C followed by the value of the text variable"""
+def c_text(text):
+    """Display “C ” followed by the value of the text variable."""
     return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def pythontext(text="is cool"):
-    """Function that displays Python followed by value of text variable"""
+def python_text(text="is cool"):
+    """Display “Python ” followed by the value of the text variable."""
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def isnum(n):
-    """Function that displays n is a number only if n is an integer"""
+def is_number(n):
+    """Display “n is a number” only if n is an integer."""
     if isinstance(n, int):
         return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n=None):
-    """Function that displays an HTML page only if n is an integer"""
+def number_template(n):
+    """Display an HTML page only if n is an integer."""
     if isinstance(n, int):
-        return render_template("5-number.html", value=n)
+        return render_template('5-number.html', value=n)
 
 
 if __name__ == "__main__":
