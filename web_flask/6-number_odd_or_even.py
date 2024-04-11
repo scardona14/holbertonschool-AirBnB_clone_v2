807f69f9bbd9e1ce3a0a487a7943db9e66a006fa
scardona14 <cardanasean123@gmail.com>
2024-04-11 14:28:45 -0400

    Added a whiteline at the bottom of the code

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Start web application with two routings
 """
 
@@ -9,56 +10,56 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    """Return string when route queried
-    """
-    return 'Hello HBNB!'
+  """Return string when route queried
+  """
+  return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Return string when route queried
-    """
-    return 'HBNB'
+  """Return string when route queried
+  """
+  return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """Return reformatted text
-    """
-    return 'C ' + text.replace('_', ' ')
+  """Return reformatted text
+  """
+  return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
-    return 'Python ' + text.replace('_', ' ')
+  """Reformat text based on optional variable
+  """
+  return 'Python ' + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
 def number(n=None):
-    """Allow request if path variable is a valid integer
-    """
-    return str(n) + ' is a number'
+  """Allow request if path variable is a valid integer
+  """
+  return str(n) + ' is a number'
 
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    """Retrieve template for request
-    """
-    path = '5-number.html'
-    return render_template(path, n=n)
+  """Retrieve template for request
+  """
+  path = '5-number.html'
+  return render_template(path, n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    """Render template based on conditional
-    """
-    path = '6-number_odd_or_even.html'
-    return render_template(path, n=n)
+  """Render template based on conditional
+  """
+  path = '6-number_odd_or_even.html'
+  return render_template(path, n=n)
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+  app.url_map.strict_slashes = False
+  app.run(host='0.0.0.0', port=5000)
