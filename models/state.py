7b899f47578a0ee0987461119cf75da9173791c1
scardona14 <cardanasean123@gmail.com>
2024-04-11 14:46:22 -0400

    Fixed errors in the code that were causing the program to crash when the user tried to enter a number that was not an integer.

--- models/state.py
+++ models/state.py
@@ -1,24 +1,37 @@
 #!/usr/bin/python3
-"""This module defines the State class"""
+"""This is the state class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import environ
 
 
 class State(BaseModel, Base):
-    """This class defines the states table"""
+    """This is the class for State
+    Attributes:
+        __tablename__: name of MySQL table
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
 
-    @property
-    def cities(self):
-        """Getter method to return the list of City objects linked to the current State"""
-        from models import storage
-        city_objs = []
-        cities_dict = storage.all(City)
-        for city in cities_dict.values():
-            if city.state_id == self.id:
-                city_objs.append(city)
-        return city_objs
+    if environ['HBNB_TYPE_STORAGE'] == 'db':
+        cities = relationship('City', cascade='all, delete', backref='state')
+    else:
+        @property
+        def cities(self):
+            """Getter method for cities
+            Return: list of cities with state_id equal to self.id
+            """
+            from models import storage
+            from models.city import City
+            # return list of City objs in __objects
+            cities_dict = storage.all(City)
+            cities_list = []
 
+            # copy values from dict to list
+            for city in cities_dict.values():
+                if city.state_id == self.id:
+                    cities_list.append(city)
+
+            return cities_list
