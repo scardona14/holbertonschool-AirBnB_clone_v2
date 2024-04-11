aebc3c3d45a2f522a1ccabc0afc9a8917d811dbf
scardona14 <cardanasean123@gmail.com>
2024-04-11 13:45:56 -0400

    Making the code more readable and maintainable

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
+
+"""Start web application with two routings."""
 
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    """Return string when route queried
-    """
+    """Return string when route queried."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb():
-    """Return string when route queried
-    """
+    """Return string when route queried."""
     return 'HBNB'
 
 
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """Return reformatted text
-    """
+    """Return reformatted text."""
     return 'C ' + text.replace('_', ' ')
 
 
 @app.route('/python/')
 @app.route('/python/<text>')
 def python_with_text(text='is cool'):
-    """Reformat text based on optional variable
-    """
+    """Reformat text based on optional variable."""
     return 'Python ' + text.replace('_', ' ')
 
 
